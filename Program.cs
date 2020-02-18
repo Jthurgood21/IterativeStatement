490640d6cc6932c022b2f0dd72d24ddb8435a2f7
Jeffery Thurgood <jthurgood@usf.edu>
2020-02-18 16:14:21 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace IterativeStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Interger: ");
+            int userNumber;
+
+            while (!int.TryParse(Console.ReadLine(), out userNumber))
+            {
+                Console.Clear();
+                Console.WriteLine("Enter only a number please!");
+            }
+            
+            if
+        }
+    }
+}
