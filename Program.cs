3f22d6b9865d369b7b2345861335e8eb5362c6ad
Jeffery Thurgood <jthurgood@usf.edu>
2020-02-18 21:15:15 -0500

    Change from while loop to for loop and fix spacing

--- Program.cs
+++ Program.cs
@@ -6,16 +6,27 @@ namespace IterativeStatement
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter Interger: ");
-            int userNumber;
 
-            while (!int.TryParse(Console.ReadLine(), out userNumber))
+            Console.Write("Enter an Interger between 2 and 10: ");
+
+            string userInput = Console.ReadLine();
+            int userNumber = int.Parse(userInput);
+
+            if (userNumber % 2 == 0)
             {
-                Console.Clear();
-                Console.WriteLine("Enter only a number please!");
+                for(int i = 1; i < userNumber + 2; i++)
+                {
+                    Console.WriteLine(i.ToString() + "." + "you entered an even number");
+                }
             }
-            
-            if
+            else
+            {
+                for (int i = 1; i < userNumber + 0; i++)
+                {
+                    Console.WriteLine(i.ToString() + "." + "You entered an odd number");
+                }
+
+            }
         }
     }
 }
